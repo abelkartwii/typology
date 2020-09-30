78956e10eb2922115dbc3f0e8c88c6575b384b89
abel kartawinata! <49835391+abelkartwii@users.noreply.github.com>
2020-09-30 14:17:35 +0700

    added basic bars

--- personality.r
+++ personality.r
@@ -1,5 +1,5 @@
-install.packages(c("ggplot2"))
-library(c("ggplot2","stats"))
+install.packages(c("dplyr","ggplot2","janitor"))
+library(c("dplyr","ggplot2","janitor", "stats"))
 
 #import data, remove unneeded data, change as factor
 personality <- read.csv(file.choose(), header = TRUE, stringsAsFactors = FALSE)
@@ -27,15 +27,57 @@ personality$wing <- sapply(
 )
 personality$wing <- as.factor(personality$wing)
 
-#transformation done, apply mask :)
-attach(personality)
+#extract MBTI function pairs + extraversion + p/j
+mbti.pairs <- substr(mbti, 2, 3)
+personality$mbti.pairs <- as.factor(mbti.pairs)
 
-# --- enneagram --- #
-# barchart
+mbti.ei <- substr(mbti, 1, 1)
+personality$mbti.ei <- as.factor(mbti.ei)
+
+mbti.pj <- substr(mbti, 4, 4)
+personality$mbti.pj <- as.factor(mbti.pj)
+
+#apply mask + mutate
+attach(personality)
+personality <- mutate(
+  mbti.pj = substr(mbti, 4, 4)
+)
+
+### --- enneagram --- ###
+## enneagram-mbti barcharts
+# stack / fill
 bar.ennea <- ggplot(personality, aes(enneagram)) +
   geom_bar(mapping = aes(enneagram, fill = mbti),
            stat = "count", position = "stack")
-bar.ennea <- ggplot(personality, aes(enneagram)) +
-  geom_bar(mapping = aes(enneagram, fill = mbti),
-           stat = "count", position = "fill")
 bar.ennea
+
+# mbti-enneagram 
+bar.mbtien <- ggplot(personality, aes(mbti)) +
+  geom_bar(mapping = aes(mbti, fill = enneagram),
+           stat = "count", position = "stack")
+bar.mbtien
+
+## enneagram-function part
+bar.enfp <- ggplot(personality, aes(enneagram)) +
+  geom_bar(mapping = aes(enneagram, fill = mbti.pairs),
+           stat = "count", position = "fill")
+bar.enfp
+
+# reverse
+bar.enfp <- ggplot(personality, aes(mbti.pairs)) +
+  geom_bar(mapping = aes(mbti.pairs, fill = enneagram),
+           stat = "count", position = "fill")
+bar.enfp
+
+## hogwarts-enneagram barchart ##
+# stack / fill
+bar.enneahouse <- ggplot(personality, aes(enneagram)) +
+  geom_bar(mapping = aes(enneagram, fill = house),
+           stat = "count", position = "stack")
+bar.enneahouse
+
+# reverse?
+bar.enneahouse <- ggplot(personality, aes(house)) +
+  geom_bar(mapping = aes(house, fill = enneagram),
+           stat = "count", position = "fill")
+bar.enneahouse
